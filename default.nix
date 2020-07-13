58ec405a409e8da7e04ada82661425a057bf3d85
Andreas Rammhold <andreas@rammhold.de>
2020-07-13 21:29:55 +0200

    Use patched lockfile approach as all the alternatives seem wonky

--- default.nix
+++ default.nix
@@ -5,17 +5,20 @@
   /* required input parameters */
 , sourcePath # path to the root of the sources for this node package
 , lockFilePath ? sourcePath + "/package-lock.json" # path of the lock file
+, packageOverride ? name: spec: {},
 }:
 let
   lockFile = builtins.fromJSON (builtins.readFile lockFilePath);
   dependencies = lockFile.dependencies or { };
-  mkSource = _: v: pkgs.fetchurl { url = v.resolved; hash = v.integrity; };
+  mkSource = name: v: if (v.resolved or "") == "" then null else pkgs.fetchurl { url = builtins.trace (name + " " + v.resolved) v.resolved; hash = v.integrity; };
   sources = lib.mapAttrs (mkSource) dependencies;
 
-  findBestSource = sourcesList: name: let
-    candidatese = lib.filter (l: builtins.hasAttr name l) sourcesList;
-    best = lib.head candidatese;
-  in best.${name};
+  findBestSource = sourcesList: name:
+    let
+      candidatese = lib.filter (l: builtins.hasAttr name l && builtins.trace ("'" + (l.name or "N/A") + "'") (l.${name} != "")) sourcesList;
+      best = lib.head candidatese;
+    in
+    best.${name};
 
   mkNode2NixDependency = previous: sourcesList: name: dep: builtins.trace [ name dep ] (
     let
@@ -49,10 +52,65 @@ let
     bypassCache = true;
     production = true;
   };
+
+  patchedLockfile = pkgs.writeTextFile {
+    name = "package-lock.json";
+    destination = "/package-lock.json";
+    text = (
+      let
+        patchDep = sourcesList: name: v:
+          let
+            dsources = lib.mapAttrs (mkSource) (v.dependencies or { });
+            src = findBestSource sourcesList name;
+          in
+          (v // {
+            dependencies = lib.mapAttrs (patchDep ([ dsources ] ++ sourcesList)) (v.dependencies or { });
+          }) // (if src == null then packageOverride name v else {
+            resolved = "file://" + (toString src);
+          }
+          );
+
+        file = lockFile // {
+          dependencies = lib.mapAttrs (patchDep [ sources ]) (lockFile.dependencies or { });
+        };
+      in
+      builtins.toJSON file
+    );
+  };
+
+  patchedBuildRoot = pkgs.symlinkJoin {
+    name = "patched-root-${lockFile.name}";
+    paths = [
+      patchedLockfile
+      sourcePath
+    ];
+  };
+
+  patchedBuild = pkgs.stdenv.mkDerivation {
+    name = lockFile.name;
+    src = patchedBuildRoot;
+    buildInputs = [ nodejs ];
+
+    buildPhase = ''
+      export HOME=$(mktemp -d)
+
+      chmod -R +rw .
+
+      npm --offline install
+    '';
+
+    installPhase = ''
+      ls -la
+      mkdir $out
+      cp -rv node_modules $out/node_modules
+    '';
+
+    passthru.lockFile = patchedLockfile + "/package-lock.json";
+  };
 in
 {
   dependenciesJson = builtins.toJSON dependencies;
-  inherit dependencies;
+  inherit dependencies patchedLockfile patchedBuildRoot patchedBuild;
   allSources = pkgs.linkFarmFromDrvs "dependencies-for-${lockFile.name}" (lib.attrValues sources);
   shell = nodeEnv.buildNodeShell args;
   tarball = nodeEnv.buildNodeSourceDist args;
